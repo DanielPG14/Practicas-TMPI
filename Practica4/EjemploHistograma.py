9d2e3d70ba83dfcc04b747b05ca39b9cbbe3f905
Daniel Pineda <daniel.pineda.gomez14@gmail.com>
2025-02-27 15:37:02 -0600

    VH.02

--- Practica4/EjemploHistograma.py
+++ Practica4/EjemploHistograma.py
@@ -7,11 +7,27 @@ ret,frame=video.read()
 imagen_gris=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 grisB=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-#Método para calculo de histograma
-hist=cv2.calcHist([grisB],[0], None, [256], [0,256])
+if video is None:
+    print("Sin video")
+else:
+    while True:
+        #Método para calculo de histograma
+        hist=cv2.calcHist([grisB],[0], None, [256], [0,256])
+        #Método para limpiar el grafico
+        plt.clf()
+        #Metodo para hacer la grafica
+        plt.plot(hist)
+        plt.xlim([0,256])
+        plt.title("Histograma")
+        plt.xlabel("Intensidad de pixel")
+        plt.ylabel("Frecuencia")
+        #plt.hist(frame.ravel(),256,[0,256])
+        #Metodo para mostrar la grafica
+        plt.pause(0.001)
+        cv2.imshow("video",frame)
+        key=cv2.waitKey(1)
+        if key==ord('q'):
+            break
 
-#Función para hacer la grafica del histograma
-#plt.plot(hist)
-plt.hist(frame.ravel(),256,[0,256])
-#Función para mostrar la grafica
-plt.show()
+video.release()
+cv2.destroyAllWindows()
