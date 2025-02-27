30b7ce243d7637f2587d9660c6ad05666f3c2ad3
DanielPG <daniel.pineda.gomez14@gmail.com>
2025-02-26 22:47:38 -0600

    Histograma con video

--- Practica4/EjemploHistograma.py
+++ Practica4/EjemploHistograma.py
@@ -2,14 +2,16 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-imagen=cv2.imread("Practica4/personas.jpg")
-imagen_gris=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+video = cv2.VideoCapture(0)
+ret,frame=video.read()
+imagen_gris=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+grisB=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 #Método para calculo de histograma
-hist=cv2.calcHist([imagen_gris],[0], None, [256], [0,256])
+hist=cv2.calcHist([grisB],[0], None, [256], [0,256])
 
 #Función para hacer la grafica del histograma
 #plt.plot(hist)
-plt.hist(imagen.ravel(),256,[0,256])
+plt.hist(frame.ravel(),256,[0,256])
 #Función para mostrar la grafica
 plt.show()
