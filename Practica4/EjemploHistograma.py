ed61122619b1d89708aca66dde802f443dc6c75a
Daniel Pineda <daniel.pineda.gomez14@gmail.com>
2025-02-27 16:19:04 -0600

    VH.03

--- Practica4/EjemploHistograma.py
+++ Practica4/EjemploHistograma.py
@@ -3,31 +3,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 video = cv2.VideoCapture(0)
-ret,frame=video.read()
-imagen_gris=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-grisB=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+plt.ion()#Activa el modo interactivo
+fig,ax=plt.subplots()#Se inicializa la grafica
 
 if video is None:
     print("Sin video")
 else:
     while True:
+        ret,frame=video.read()
+        grisB=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         #Método para calculo de histograma
         hist=cv2.calcHist([grisB],[0], None, [256], [0,256])
-        #Método para limpiar el grafico
-        plt.clf()
+        #limpiar el grafico
+        ax.clear()
         #Metodo para hacer la grafica
-        plt.plot(hist)
-        plt.xlim([0,256])
-        plt.title("Histograma")
-        plt.xlabel("Intensidad de pixel")
-        plt.ylabel("Frecuencia")
+        ax.plot(hist)
+        ax.set_xlim([0,256])
+        ax.set_title("Histograma")
+        ax.set_xlabel("Intensidad de pixel")
+        ax.set_ylabel("Frecuencia")
         #plt.hist(frame.ravel(),256,[0,256])
         #Metodo para mostrar la grafica
         plt.pause(0.001)
-        cv2.imshow("video",frame)
+        cv2.imshow("video",grisB)
         key=cv2.waitKey(1)
         if key==ord('q'):
             break
 
 video.release()
 cv2.destroyAllWindows()
+plt.ioff()#Desactiva el modo interactivo
+plt.show()#Ultia grafica
